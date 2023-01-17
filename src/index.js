c0b009f8f60c567d148212643aee8c65cebed953
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-17 08:57:13 -0400

    Add in index.js, an array and populate index.html with the elements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import './style.css';
 
-const btn = document.getElementById('btn');
-function btnClickHandler() {
-  const text = document.createElement('p');
-  text.innerHTML = 'Hello';
-  document.body.appendChild(text);
-}
-btn.addEventListener('click', btnClickHandler);
+const list = document.getElementById('list');
+
+const arr = [
+  {
+    description: 'Lorem, ipsum dolor.',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Lorem, ipsum dolor.',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Lorem, ipsum dolor.',
+    completed: false,
+    index: 3,
+  },
+];
+
+const appendToDOM = (todo) => {
+  const element = document.createElement('li');
+  element.setAttribute('id', todo.index);
+  element.innerHTML = `
+  <input type="checkbox" ${todo.completed ? 'checked' : ''}/>
+  <span>${todo.description}</span>`;
+  list.appendChild(element);
+};
+
+const loadElements = () => {
+  arr.forEach((todo) => {
+    appendToDOM(todo);
+  });
+};
+
+window.onload = () => {
+  loadElements();
+};
