5ce6c27413ddb9cfdfb4a548fe268474a2de5365
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-18 08:45:22 -0400

    Remove unneeded action removeFromDom in trashHandler

--- src/modules/trashHandler.js
+++ src/modules/trashHandler.js
@@ -1,10 +1,8 @@
 import Todo from './todo.js';
-import { removeFromDOM } from './handleDOM.js';
 
 const trashEventHandler = (e) => {
   const todo = e.target.parentElement;
   const indexTodo = todo.getAttribute('id');
-  removeFromDOM(todo);
   Todo.removeTodo(indexTodo);
 };
 
