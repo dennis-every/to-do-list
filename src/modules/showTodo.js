493d8358b7c335e26fb0a2548299f4d0eaaf3863
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-18 15:15:03 -0400

    Add event listener to checkbox, changeCheckboxHandler, and refactored create show element

--- src/modules/showTodo.js
+++ src/modules/showTodo.js
@@ -2,28 +2,48 @@ import Menu from '../images/ellipsis-vertical.svg';
 import createEditFormElement from './editTodoForm.js';
 import updateTodoHandler from './updateHandler.js';
 
+const changeCheckboxHandler = (e) => {
+  console.log(e.target);
+};
+
+const clickMenuHandler = (e) => {
+  const showElement = e.target.parentElement;
+  const indexTodo = showElement.getAttribute('id');
+  const editElement = createEditFormElement(indexTodo);
+  editElement.addEventListener('submit', updateTodoHandler);
+  const todoList = showElement.parentElement;
+  todoList.replaceChild(editElement, showElement);
+};
+
+const createCheckboxElement = () => {
+  const checkboxElement = document.createElement('input');
+  checkboxElement.setAttribute('class', 'checkbox');
+  checkboxElement.setAttribute('type', 'checkbox');
+  checkboxElement.addEventListener('change', changeCheckboxHandler);
+  return checkboxElement;
+};
+
+const createMenuElement = () => {
+  const menuElement = new Image();
+  menuElement.src = Menu;
+  menuElement.setAttribute('class', 'icon');
+  menuElement.addEventListener('click', clickMenuHandler);
+  return menuElement;
+};
+
 const createShowElement = (todo) => {
-  const element = document.createElement('li');
-  const menuIcon = new Image();
-  menuIcon.src = Menu;
-  menuIcon.setAttribute('class', 'icon');
-  element.setAttribute('id', todo.index);
-  element.innerHTML = `
-  <label>
-    <input class="checkbox" type="checkbox" ${todo.completed ? 'checked' : ''}/>
-    <span>${todo.description}</span>
-  </label
-  `;
-  element.appendChild(menuIcon);
-  menuIcon.addEventListener('click', (e) => {
-    const showElement = e.target.parentElement;
-    const indexTodo = showElement.getAttribute('id');
-    const editElement = createEditFormElement(indexTodo);
-    editElement.addEventListener('submit', updateTodoHandler);
-    const todoList = showElement.parentElement;
-    todoList.replaceChild(editElement, showElement);
-  });
-  return element;
+  const showElement = document.createElement('li');
+  showElement.setAttribute('id', todo.index);
+  const labelElement = document.createElement('label');
+  const checkboxElement = createCheckboxElement();
+  labelElement.appendChild(checkboxElement);
+  const descriptionElement = document.createElement('span');
+  descriptionElement.innerText = todo.description;
+  labelElement.appendChild(descriptionElement);
+  showElement.appendChild(labelElement);
+  const menuElement = createMenuElement();
+  showElement.appendChild(menuElement);
+  return showElement;
 };
 
 export default createShowElement;
