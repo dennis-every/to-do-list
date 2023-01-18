f03dec67dfdb416411bb2dcbf8f5a13a5b259af4
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-18 14:28:07 -0400

    Add module completionHandler.js

---
+++ src/modules/completionHandler.js
@@ -0,0 +1,9 @@
+import Todo from './todo.js';
+
+export const setCompletedToTrue = (todoIndex) => {
+  Todo.getTodo(todoIndex);
+};
+
+export const setCompletedToFalse = (todoIndex) => {
+  Todo.getTodo(todoIndex);
+};
