bfdec34e32e200de3c94625ced7422217d3d86cf
eNeville <17412822+eNeville@users.noreply.github.com>
2023-01-17 22:53:08 -0400

    Set the index of a new todo to todosArray.length + 1

--- src/modules/newTodoForm.js
+++ src/modules/newTodoForm.js
@@ -1,15 +1,17 @@
 import Todo from './todo.js';
 import { appendToDOM } from './handleDOM.js';
 import createShowElement from './showTodo.js';
+import { retrieveData } from './handleData.js';
 
 export const newTodoForm = document.getElementById('todo-form');
 
 export const newTodoFormHandler = (e) => {
   e.preventDefault();
+  const todosArray = retrieveData();
   const newTodo = new Todo(
     newTodoForm.elements['add-todo'].value,
     false,
-    new Date().valueOf(),
+    todosArray.length + 1,
   );
   Todo.addTodo(newTodo);
   const todoElement = createShowElement(newTodo);
